4033f1f6605cb0a84d27e69a6dc57ae926c9fac3
Alexander Norberg <ackess@gmail.com>
2021-04-17 16:52:10 +0200

    Fixed tests to be pass only, input value and expected output is given and tests test for that.

--- lab1.py
+++ lab1.py
@@ -9,57 +9,49 @@
 #!/usr/bin/env python3
 
 def testcases():
-    primepass = []
-    eeapass = []
-    phipass = []
-    invpass = []
-    primefail = []
-    eeafail = []
-    phifail = []
-    invfail = []
+    primet = []
+    eeat = []
+    phit = []
+    invt = []
+
     with open('testcases.txt', mode='r', encoding='UTF8') as reader:
         a = reader.readlines()
         for line in a:
-            if line[0] == '#':
-                continue
-            v = line.split(',')
-            if v[0] == 'eea':
-                l = list(map(lambda x : int(x),v[2:5]))
-                if v[1] == 'pass':
-                    eeapass.append(l)
-                elif v[1] == 'fail':
-                    eeafail.append(l)
-            elif v[0] == 'phi':
-                l = list(map(lambda x: int(x), v[2:4]))
-                if v[1] == 'pass':
-                    phipass.append(l)
-                elif v[1] == 'fail':
-                    phifail.append(l)
-            elif v[0] == 'inverse':
-                l = list(map(lambda x: int(x), v[2:5]))
-                if v[1] == 'pass':
-                    invpass.append(l)
-                elif v[1] == 'fail':
-                    invfail.append(l)
-            elif v[0] == 'prime':
-                p = int(v[2])
-                if v[1] == 'pass':
-                    primepass.append(p)
-                elif v[1] == 'fail':
-                    primefail.append(p)    
-    return primepass, eeapass, phipass, invpass, primefail, eeafail, phifail, invfail
+            try:
+                if line[0] == '#':
+                    continue
+                v = line.split(',')
+                if v[0] == 'eea':
+                    l = list(map(lambda x : int(x),v[1:4]))
+                    eeat.append(l)
+                elif v[0] == 'phi':
+                    l = list(map(lambda x: int(x), v[1:3]))
+                    phit.append(l)
+                elif v[0] == 'inverse':
+                    l = list(map(lambda x: int(x), v[1:4]))
+                    invt.append(l)
+                elif v[0] == 'prime':
+                    p = int(v[1])
+                    b = v[2].lower().strip()
+                    # If b string is "true", res evaluates boolean value true, otherwise false.
+                    res = (b == 'true')
+                    primet.append([p, res])
+            except ValueError:
+                print(f"Format error, skipping line: {line}")
+    return primet, eeat, phit, invt
 
 """
 Task 1
 The code below represents a loop of finding out if the given integer "num" is a prime number or not.
 Given the integer, the code if-statement checks if the "num" is greater then 1, if it is, it checks if it is a prime number.
+Source description: https://en.wikipedia.org/wiki/Primality_test
 """
 def is_prime(num):
     if num <= 1 or num % 2 == 0:
         return False
     if num == 2 or num == 3:
         return True
-    for i in range(3,int(num**0.5),2):
+    for i in range(3,int(num**0.5)+1,2):
         if (num % i) == 0:
             return False
     return True
@@ -67,6 +59,7 @@ def is_prime(num):
 """
 Task 2
 The code below represents a function to check if the two values are relatively prime.
+Source description: https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
 """
 def EEA(a,b):
     old_r, r = a,b
@@ -84,6 +77,7 @@ def EEA(a,b):
 """
 Task 3
 The code below represents the Euler phi function.
+Source description: https://en.wikipedia.org/wiki/Euler%27s_totient_function
 """
 def phi(n):
     r = 0
@@ -97,12 +91,8 @@ def phi(n):
 Task 4
 The code below represent the function for calculating the modular inverse
 and to check if the value is invertible.
+Source description: https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Computing_multiplicative_inverses_in_modular_structures
 """
-def big_first(a,b):
-    if a > b:
-        return a,b
-    else:
-        return b,a
 
 def modular_inverse(a,n):
     if (EEA(a,n) != 1):
@@ -158,74 +148,58 @@ def run_inverse():
     input()
 
 def run_tests():
+    n_errors = 0
     #menu()
-    primepass, eeapass ,phipass, invpass, primefail, eeafail, phifail, invfail = testcases()
+    primetests, eeatests ,phitests, invtests = testcases()
 
     #Test prime
     print("Testing primality test - tests should pass")
-    for p in primepass:
-        evald = is_prime(p)
-        if evald:
-            print(f"PASSED - is_prime({p})={evald}.")
+    for p in primetests:
+        # p has format [int,bool] where int is the prime value
+        # and the bool is the expected boolean value.
+        evald = is_prime(p[0])
+        if evald == p[1]:
+            print(f"PASSED - is_prime({p[0]})={evald}.")
         else:
-            print(f"FAILED - is_prime({p})={evald} should be True.")
-    for p in primefail:
-        evald = is_prime(p)
-        if evald:
-            print(f"FAILED - is_prime({p})={evald} - should be False.")
-        else:
-            print(f"PASSED - is_prime({p})={evald}.")
+            n_errors += 1
+            print(f"FAILED - is_prime({p[0]})={evald} should be {p[1]}.")
+
     #Test EEA
     print("\nTesting EEA algorithm - tests should pass:")
-    for t in eeapass:
+    for t in eeatests:
         evald = EEA(t[0],t[1])
         if evald == t[2]:
             print(f"PASSED - EEA({t[0]},{t[1]})={evald}")
+          
         else:
+            n_errors += 1
             print(f"FAILED - EEA({t[0]},{t[1]})={evald} expected {t[2]}")
-
-    print("\nTesting EEA algorithm - tests should fail:")
-    for t in eeafail:
-        evald = EEA(t[0],t[1])
-        if evald == t[2]:
-            print(f"FAILED - EEA({t[0]},{t[1]})={t[2]} function gave {evald}")
-        else:
-            print(f"PASSED - EEA({t[0]},{t[1]})={t[2]} function gave {evald}")
-
+    
     #Test Phi
     print("\nTesting Phi algorithm - tests should pass:")
-    for t in phipass:
+    for t in phitests:
         evald = phi(t[0])
         if evald == t[1]:
             print(f"PASSED - phi({t[0]})={evald}")
         else:
+            n_errors += 1
             print(f"FAILED - phi({t[0]})={evald} expected {t[1]}")
             
-    print("\nTesting Phi algorithm - tests should fail:")
-    for t in phifail:
-        evald = phi(t[0])
-        if evald == t[1]:
-            print(f"FAILED - phi({t[0]})={t[1]} function gave {evald}")
-        else:
-            print(f"PASSED - phi({t[0]})={t[1]} function gave {evald}")
-
     #Test Inverse
     print("\nTesting Inverse algorithm - tests should pass:")
-    for t in invpass:
+    for t in invtests:
         evald = modular_inverse(t[0], t[1])
         if evald == t[2]:
             print(f"PASSED - inverse({t[0]},{t[1]})={evald}")
         else:
+            n_errors += 1
             print(f"FAILED - inverse({t[0]},{t[1]})={evald} expected {t[2]}")
-            
-    print("\nTesting Inverse algorithm - tests should fail:")
-    for t in invfail:
-        evald = modular_inverse(t[0], t[1])
-        if evald == t[2]:
-            print(f"FAILED - inverse({t[0]},{t[1]})={t[2]} function gave {t[2]}")
-        else:
-            print(f"PASSED - inverse({t[0]},{t[1]})= Inverse: not available, function gave {t[2]} ")
 
+    if n_errors >= 1:
+        print(f"Got {n_errors} failed tests.")
+    else:
+        print("All tests passed!")
+            
     print("Tests are finished running - press any key to go back to menu.")
     input()
 
@@ -233,32 +207,35 @@ def menu():
     option = 0
     while option != -1:
         print("------------------ Lab 1: RSA toolbox ----------------------------")
-        print("[1] Task 1")
-        print("[2] Task 2")
-        print("[3] Task 3")
-        print("[4] Task 4")
-        print("[5] Task 5")
+        print("[1] Task 1 - Prime numbers")
+        print("[2] Task 2 - EEA")
+        print("[3] Task 3 - phi")
+        print("[4] Task 4 - inverse")
+        print("[5] Task 5 - automated tests")
         print("------------------------------------------------------------------")
 
-        option = int(input("Pick a task (-1 to quit): "))
-        if option >= 1 and option <= 5:
-            if option == 1:
-                run_prime()
-            elif option == 2:
-                run_eea()
-            elif option == 3:
-                run_phi()
-            elif option == 4:
-                run_inverse()
+        try:
+            option = int(input("Pick a task (-1 to quit): "))
+            if option >= 1 and option <= 5:
+                if option == 1:
+                    run_prime()
+                elif option == 2:
+                    run_eea()
+                elif option == 3:
+                    run_phi()
+                elif option == 4:
+                    run_inverse()
+                else:
+                    run_tests()
             else:
-                run_tests()
-        else:
-            print("Invalid choice - pick again.")    
+                print("Invalid choice - pick again.")
+        except ValueError:
+            print("Not a valid input, only numbers allowed!") 
 
 if __name__ == "__main__":
     menu()
 
-############################## Question ###################################################################################
+############################## Questions ###################################################################################
 """
 Question 1: How does the factorization problem relate to the RSA security?
             In other words, how can infeasibility of finding the prime factors of N help with the security of RSA?
@@ -275,5 +252,6 @@ Answer:     Depends on how big the number is, if it's a 3-digit number one need 
 
 Question 3: What edge conditions have you tested using your automated test cases?
 
-Answer:     
+Answer:     We have tested for negative values which in general should not work and also checked for
+            known values which we know the answer to.
 """
